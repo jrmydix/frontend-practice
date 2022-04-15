efe88d3600573ba6ef44f5956726fadbc5427313
Jérémy <50837004+jrmydix@users.noreply.github.com>
2022-04-15 08:53:35 +0200

    calculator-app: fixed decimal calculation

--- frontend-mentor/calculator-app/js/calculator.js
+++ frontend-mentor/calculator-app/js/calculator.js
@@ -35,9 +35,9 @@ const createResultString = (key, displayedNum, state) => {
   }
 
   if (keyType === "decimal") {
-    if (!displayedNum.includes(".")) return displayedNum + ",";
+    if (!displayedNum.includes(".")) return displayedNum + ".";
     if (previousKeyType === "operator" || previousKeyType === "calculate")
-      return "0,";
+      return "0.";
     return displayedNum;
   }
 
